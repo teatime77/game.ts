3336ebe6fd04a9aaf4f2dc14c22a475ca0e6cf38
teatime77 <teatime77@live.jp>
2026-02-18 09:57:57 +0900

    outDir => ./dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
 import { defineConfig, mergeConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import { baseConfig } from '../vite.config.base';
 import { resolve } from 'path';
 
 export default defineConfig(
   mergeConfig(baseConfig, {
+    plugins: [tsconfigPaths()],
     build: {
       minify: false,
       lib: {
         entry: resolve(__dirname, 'ts/game.ts'),
-        fileName: 'index',
+        fileName: 'game',
         formats: ['es']
       },
-      outDir: './game.ts/public/lib/game'
+      outDir: '../dist'
     }
   })
 );
